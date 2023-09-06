e13e5044eac4295b4b4940a71fa65c3ea4ac0196
Blas A. L. F <blasylf@gmail.com>
2023-09-06 11:23:32 +0200

    Idk

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 
+
+
+class Car:
+    def __init__(self):
+        self.weight = 0 #Measure of how good it can accelerate/brake
+        self.vel = 0
+        self.x, self.y = 0 #y is "basically only which rijstrook
+        
+    def braking(weight, infront):
+        
+    def acel(infront?):
+        self.vel =      
+    
+    def update(self):
+        if noCarInFront:
+            #vroom
+        elif inhaalMogelijk:
+            inhaal
+        else:
+            brakeTill2SecRule
+
+class Lane:
+    def __init__(self):
+        self.maxspeed = 100/3.6
+        self.spits = False
+        self.tunnel = False
+        #Angle of incline somehow?
+    
+    
+
+
+#Road conditions? - e.g. wet
+class Simulation:
+    def __init__(self):
+        self.lanes = []
+        
